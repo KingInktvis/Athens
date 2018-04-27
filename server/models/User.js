9217def133d5debc3c9d40a3de4e4559c41d5908
KingInktvis <rikdevries253@hotmail.nl>
2018-04-27 16:40:39 +0200

    Actualy make the model and export it.

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const User = new Schema({
+const userSchema = new Schema({
     email: String,
     name: String,
     password: String,
 });
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
