8e09db7296d81c3c24bac3aba6c03e805a4fcb9d
KingInktvis <rikdevries253@hotmail.nl>
2018-05-13 16:21:34 +0200

    Added the header and materialize css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Header from './components/header';
 import Home from './components/home';
 import SignUp from './components/signup';
 import SignIn from './components/signin';
@@ -9,11 +10,14 @@ class App extends Component {
   render() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/signup' component={SignUp} />
-                <Route exact path='/signin' component={SignIn}/>
-            </Switch>
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/signup' component={SignUp} />
+                    <Route exact path='/signin' component={SignIn}/>
+                </Switch>
+            </div>
         </BrowserRouter>
     );
   }
---
+++ client/src/components/header.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import * as actions from '../actions';
+
+class Header extends Component {
+
+    signUp() {
+        return this.props.signInReducer.token ? '' : <li><Link to='/signup'>Sign up</Link></li>;
+    }
+
+    signIn() {
+        return this.props.signInReducer.token ? '' : <li><Link to='/signin'>Sign in</Link></li>;
+    }
+
+    signOut() {
+        return this.props.signInReducer.token ? <li><Link to='/signout'>Sign out</Link></li> : '';
+    }
+
+    render() {
+        return(
+            <nav>
+                <div className='nav-wrapper'>
+                    <Link to='/' className='brand-logo'>Athens</Link>
+                    <ul className='right hide-on-med-and-down'>
+                        {this.signUp()}
+                        {this.signIn()}
+                        {this.signOut()}
+                    </ul>
+
+                </div>
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        signInReducer: state.signin
+    }
+}
+
+export default connect(mapStateToProps, actions)(Header);
